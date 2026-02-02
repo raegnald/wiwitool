f9cc12fbf94638d2ef1e7460b04f206d918086c9
Ronaldo Gligan <ronaldogligan@gmail.com>
2026-02-02 13:35:47 +0100

    Create ad-hoc Minecraft identifiers, without having to inherit Minecraft_identifiable

--- wiwitool/include/Minecraft_identifiable.hpp
+++ wiwitool/include/Minecraft_identifiable.hpp
@@ -5,23 +5,32 @@
 
 #include "strutil.hpp"
 
-struct Minecraft_identifiable {
-  std::string mc_namespace;
-  std::string object_name;
-
+class Minecraft_identifiable {
+public:
   Minecraft_identifiable(void) = delete;
 
   explicit Minecraft_identifiable(std::string namespace_,
                                   std::string object_name)
       : mc_namespace{namespace_}, object_name{object_name} {}
 
-  std::string id(void) const {
-    const auto id_format = std::format("{}:{}", mc_namespace, object_name);
+  // Create a Minecraft identifier ad hoc given a namespace and an
+  // object name.
+  static std::string adhoc(std::string adhoc_namespace, std::string adhoc_name) {
+    const auto id_format = std::format("{}:{}", adhoc_namespace, adhoc_name);
     return nospaces(tolower(id_format));
   }
 
-  std::string item_id(void) const {
-    const auto id_format = std::format("{}:item/{}", mc_namespace, object_name);
+  // Create a Minecraft identifier for an inventory item ad hoc given
+  // a namespace and an object name.
+  static std::string adhoc_item(std::string adhoc_namespace, std::string adhoc_name) {
+    const auto id_format = std::format("{}:item/{}", adhoc_namespace, adhoc_name);
     return nospaces(tolower(id_format));
   }
+
+  std::string id(void) const { return adhoc(mc_namespace, object_name); }
+  std::string item_id(void) const {return adhoc_item(mc_namespace, object_name); }
+
+private:
+  std::string mc_namespace;
+  std::string object_name;
 };
