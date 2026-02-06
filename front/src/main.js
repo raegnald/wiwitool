f5dcd6d2587f90990ee6599b3fd54ea9aca9524f
Ronaldo Gligan <ronaldogligan@gmail.com>
2026-02-06 19:20:22 +0100

    Remove old Svelte front-end

--- front/src/main.js
+++
@@ -1,9 +0,0 @@
-import { mount } from 'svelte'
-import './app.css'
-import AppInit from './AppInit.svelte'
-
-const app = mount(AppInit, {
-  target: document.getElementById('app'),
-})
-
-export default app
