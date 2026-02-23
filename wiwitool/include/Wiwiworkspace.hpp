f9d6164989f228eeece12caaecebd5b2d67caf5d
Ronaldo Gligan <ronaldogligan@gmail.com>
2026-02-23 18:52:58 +0100

    Create Wiwiworkspace class

---
+++ wiwitool/include/Wiwiworkspace.hpp
@@ -0,0 +1,38 @@
+// A wrapper, generator and source of truth for all the Minecraft
+// packs.
+
+#pragma once
+
+#include "paintings/Painting.hpp"
+
+#include <filesystem>
+#include <memory>
+#include <vector>
+
+class Wiwiworkspace {
+public:
+  Wiwiworkspace(void) = default;
+
+  // Paintings
+
+  void add_painting(std::shared_ptr<Painting>);
+  void remove_painting(std::string id);
+
+  std::vector<std::shared_ptr<Painting>> get_paintings(void) const;
+
+  // Invisible item frames
+
+  void set_invisible_item_frames(bool);
+  bool get_invisible_item_frames() const;
+
+  // Serialisation and generation
+
+  std::string serialise(void) const;
+  void deserialise(const std::string &json_string);
+
+  std::filesystem::path generate_zip(void);
+
+private:
+  std::vector<std::shared_ptr<Painting>> paintings;
+  bool invisible_item_frames{false};
+};
