bc0def610290708cb5e00db0c163e0b574033b01
Ronaldo Gligan <ronaldogligan@gmail.com>
2026-03-26 09:29:40 +0100

    Add descriptive text to guide users in using the paintings tool

--- front/src/paintings/HelpUsingPaintingsTool.svelte
+++ front/src/paintings/HelpUsingPaintingsTool.svelte
@@ -1,4 +1,45 @@
+<script>
+  import {
+    CopyIcon,
+    CropIcon,
+    FolderInputIcon,
+    ImageUpIcon,
+    RotateCcwIcon,
+    Trash2Icon,
+  } from "@lucide/svelte";
+</script>
+
 <div>
+  <h3>How to generate your paintings</h3>
+  <ol>
+    <li>
+      <b>Import your files.</b>
+      The first step is selecting which pictures you want to turn into paintings and
+      importing the to the Wiwitool. For that, either drag and drop them into the
+      images import zone (<ImageUpIcon size="1.1em" />) or select them from your
+      computer using the file picker button (<FolderInputIcon size="1.1em" />).
+    </li>
+    <li>
+      <b>Edit your souce images.</b>
+      Now is when the fun begins! Once your images have loaded into the Wiwitool you
+      can edit them to make the look even better in-game. On the left hand side of
+      the painting card you have a few tools at your disposal: you can rotate the
+      image (<RotateCcwIcon size="1.1em" />) or crop it (<CropIcon
+        size="1.1em"
+      />). If you want multiple crops of the same image you can clone it (<CopyIcon
+        size="1.1em"
+      />), and if you change your mind you can delete it (<Trash2Icon
+        size="1.1em"
+      />).
+    </li>
+
+    <li>
+      <b>Edit your paintings.</b>
+      Now its time to get into the minecrafty stuff, on the right of the painting
+      card you have options to change the title (...) and author (...) of the painting
+      in-game.
+    </li>
+  </ol>
   <p>
     This is a web tool for creating <b
       >custom painting items in Minecraft out of your own images</b
